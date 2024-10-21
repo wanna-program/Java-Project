8172a189896f39ea4b82d7f9037f735a165ed8f4
Dimitris <d_zachos@outlook.com>
2024-10-21 22:30:06 +0300

    Added abstract to Person class to encapsulate common attributes for customer-related entities.

--- src/com/travelcompany/eshop/models/Person.java
+++ src/com/travelcompany/eshop/models/Person.java
@@ -1,4 +1,41 @@
 package com.travelcompany.eshop.models;
 
-public class Person {
+public abstract class Person {
+
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+    private String nationality;
+
+
+    public Person(int id, String name, String email, String address, String nationality) {
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.nationality = nationality;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
 }
