ea663eb0aea9ddda36223c25f1e88bd8998f4ca5
Dimitris <d_zachos@outlook.com>
2024-10-30 15:17:17 +0200

    Interface for customer-related operations.Define CustomerService interface with required methods

--- src/com/travelcompany/eshop/services/CustomerService.java
+++ src/com/travelcompany/eshop/services/CustomerService.java
@@ -1,4 +1,10 @@
 package com.travelcompany.eshop.services;
 
+import com.travelcompany.eshop.exceptions.CustomerEmailException;
+import com.travelcompany.eshop.exceptions.CustomerNotFoundException;
+import com.travelcompany.eshop.models.Customer;
+
 public interface CustomerService {
+    Customer findCustomerById(int id) throws CustomerNotFoundException;
+    void addCustomer(Customer customer) throws CustomerEmailException;
 }
