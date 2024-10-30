4d477ef88eceaaa98f98dd88b2dc26890d8aa6b4
Dimitris <d_zachos@outlook.com>
2024-10-30 15:20:57 +0200

    Interface for ticket purchasing operations. Define TicketService interface with purchaseTicket method.

--- src/com/travelcompany/eshop/services/TicketService.java
+++ src/com/travelcompany/eshop/services/TicketService.java
@@ -1,4 +1,13 @@
 package com.travelcompany.eshop.services;
 
+import com.travelcompany.eshop.models.Customer;
+import com.travelcompany.eshop.models.Itinerary;
+import com.travelcompany.eshop.models.PaymentMethod;
+import com.travelcompany.eshop.models.Ticket;
+
+import java.util.List;
+
 public interface TicketService {
+    Ticket purchaseTicket(Customer customer, Itinerary itinerary, PaymentMethod paymentMethod);
+    List<Ticket> getTickets();
 }
