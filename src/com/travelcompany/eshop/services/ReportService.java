53667b82dd4837293481d4cff9fe0cc11fd7f073
Dimitris <d_zachos@outlook.com>
2024-10-30 15:23:26 +0200

    Interface for generating reports. Define ReportService interface with report generation methods.

--- src/com/travelcompany/eshop/services/ReportService.java
+++ src/com/travelcompany/eshop/services/ReportService.java
@@ -1,4 +1,19 @@
 package com.travelcompany.eshop.services;
 
+import com.travelcompany.eshop.models.Customer;
+
+import java.util.List;
+
 public interface ReportService {
+    void generateTotalTicketsAndCostPerCustomer();
+
+    void generateTopCustomersByTicketsAndCost();
+
+    void generateCustomersWithNoTickets(List<Customer> allCustomers);
+
+    void generateItinerariesReport();
+
+    void generateTicketsWithDiscountDetails();
+
+    void generateDestinationsPerCustomer();
 }
