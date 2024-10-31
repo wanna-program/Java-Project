f47a16b088df3b88bab729f3bc2a7a33ebfb0de6
Dimitris <d_zachos@outlook.com>
2024-10-31 11:59:37 +0200

    Implement CustomerService in CustomerServiceImpl.

--- src/com/travelcompany/eshop/services/CustomerServiceImpl.java
+++ src/com/travelcompany/eshop/services/CustomerServiceImpl.java
@@ -1,4 +1,31 @@
 package com.travelcompany.eshop.services;
 
-public class CustomerServiceImpl {
+import com.travelcompany.eshop.exceptions.CustomerEmailException;
+import com.travelcompany.eshop.exceptions.CustomerNotFoundException;
+import com.travelcompany.eshop.models.Customer;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    private List<Customer> customers;
+
+    public CustomerServiceImpl(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+    @Override
+    public Customer findCustomerById(int id) throws CustomerNotFoundException {
+        return customers.stream()
+                .filter(c -> c.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new CustomerNotFoundException("Customer not found."));
+    }
+
+    @Override
+    public void addCustomer(Customer customer) throws CustomerEmailException {
+        if (customer.getEmail().endsWith("@travelcompany.com")) {
+            throw new CustomerEmailException("Email cannot end with @travelcompany.com.");
+        }
+        customers.add(customer);
+    }
 }
