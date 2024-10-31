ae5e9242c16a2ce473a6db144cdb65edccc0fcd7
Dimitris <d_zachos@outlook.com>
2024-10-31 11:51:31 +0200

    Defined ItineraryService Interface with required methods.

--- src/com/travelcompany/eshop/services/ItineraryService.java
+++ src/com/travelcompany/eshop/services/ItineraryService.java
@@ -1,10 +1,10 @@
 package com.travelcompany.eshop.services;
 
-import com.travelcompany.eshop.exceptions.InvalidAirportCodeException;
 import com.travelcompany.eshop.exceptions.ItineraryNotFoundException;
+import com.travelcompany.eshop.exceptions.InvalidAirportCodeException;
 import com.travelcompany.eshop.models.Itinerary;
 
 public interface ItineraryService {
-    Itinerary findItineraryById(int id) throws ItineraryNotFoundException;
     void addItinerary(Itinerary itinerary) throws InvalidAirportCodeException;
+    Itinerary findItineraryById(int id) throws ItineraryNotFoundException;
 }
