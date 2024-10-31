15adf84078471d6c5c3c4247f5568e5d042ddeb9
Dimitris <d_zachos@outlook.com>
2024-10-31 11:57:03 +0200

    Defined ReportService Interface with required methods.

--- src/com/travelcompany/eshop/services/ReportService.java
+++ src/com/travelcompany/eshop/services/ReportService.java
@@ -5,10 +5,12 @@ import com.travelcompany.eshop.models.Customer;
 import java.util.List;
 
 public interface ReportService {
+    void generateCustomerReport();
+    void generateItineraryReport();
+    void generateOrderedTicketsReport();
     void generateTotalTicketsAndCostPerCustomer();
-
+    void generateTotalItinerariesPerDestinationAndDeparture();
     void generateTopCustomersByTicketsAndCost();
-
     void generateCustomersWithNoTickets(List<Customer> allCustomers);
 
     void generateItinerariesReport();
